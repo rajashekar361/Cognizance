ca07214da48dd3e36b13f6cb415affa75bf82416
rajashekar361 <rajashekargottimukkula@gmail.com>
2022-06-30 17:52:35 +0530

    task6

--- task6/question_1.py
+++ task6/question_1.py
@@ -1,16 +1,16 @@
 import re,csv
 fh = open('onelinefile.txt')
 for i in fh:
-        n = re.findall(r'[+-]?[0-9]+\.[0-9]+', i)
-        a = re.findall(r'[a-zA-Z]+', i)
+        num = re.findall(r'[+-]?[0-9]+\.[0-9]+', i)
+        alpha = re.findall(r'[a-zA-Z]+', i)
         j = 0
         for p in range(len(n)):
-            with open('demo2.csv', 'a', newline='') as file:
+            with open('filename2.csv', 'a', newline='') as file:
                 writer = csv.writer(file)
-                writer.writerow([str(p+1), a[j],n[p],a[j+1]])
+                writer.writerow([str(p+1), alpha[j],num[p],alpha[j+1]])
             j += 2
 
-with open('demo.csv', 'r',) as file:
+with open('filename2.csv', 'r',) as file:
     reader = csv.reader(file)
     for row in reader:
         print(','.join(row))
